7d4ae7222e8cbf4262748c8ef03349fbe0f418d0
letienkhang <tienkhangle@gmail.com>
2022-03-23 15:58:45 +0700

    Add todolist

---
+++ lib/data/model/todo_model.dart
@@ -0,0 +1,35 @@
+class Todo {
+  String title;
+
+  Todo({required this.title});
+
+  @override
+  String toString() => 'Todo(title: $title)';
+
+  factory Todo.fromJson(Map<String, dynamic> json) => Todo(
+    title: json['title'] as String,
+  );
+
+  Map<String, dynamic> toJson() => {
+    'title': title,
+  };
+
+  Todo copyWith({
+    required String title,
+  }) {
+    return Todo(
+      title: title,
+    );
+  }
+
+  @override
+  bool operator ==(other) {
+    if (other is! Todo) {
+      return false;
+    }
+    return title == (other).title;
+  }
+
+  @override
+  int get hashCode => title.hashCode;
+}
---
+++ lib/data/todo_storage.dart
@@ -0,0 +1,52 @@
+import 'dart:convert';
+import 'dart:io';
+
+
+import 'package:path_provider/path_provider.dart';
+
+import 'model/todo_model.dart';
+
+
+final TodoStorage todoStorage = FileSystemTodoStorage();
+
+abstract class TodoStorage {
+  Future<List<Todo>> readTodoList();
+  Future<void> insertTodo(Todo todo);
+}
+
+class FileSystemTodoStorage extends TodoStorage {
+  @override
+  Future<List<Todo>> readTodoList() async {
+    try {
+      final file = await _localFile;
+      final content = await file.readAsString();
+      final List<dynamic> jsonArray = jsonDecode(content);
+      return jsonArray
+          .map(
+            (jsonObject) => Todo.fromJson(jsonObject),
+      )
+          .toList();
+    } catch (e) {
+      return [];
+    }
+  }
+
+  Future<File> get _localFile async {
+    final directory = await getApplicationDocumentsDirectory();
+    final path = directory.path;
+    return File('$path/todoList.json');
+  }
+
+  @override
+  Future<void> insertTodo(Todo todo) async {
+    final latestTodoList = await readTodoList();
+    latestTodoList.add(todo);
+    try {
+      final file = await _localFile;
+      final jsonArray = latestTodoList.map((todo) => todo.toJson()).toList();
+      await file.writeAsString(jsonEncode(jsonArray));
+    } catch (e) {
+      return;
+    }
+  }
+}
